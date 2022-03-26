d3b5f20699ec798e94d93be5ae19cabcc06c5ac6
quanjindeng <54259993+quanjindeng@users.noreply.github.com>
2022-03-27 06:54:15 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -66,19 +66,20 @@ git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-
 svn co https://github.com/immortalwrt-collections/openwrt-gowebdav/trunk/luci-app-gowebdav package/luci-app-gowebdav
 svn co https://github.com/immortalwrt-collections/openwrt-gowebdav/trunk/gowebdav package/gowebdav
 git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-app-argon
 
 
 # 删除重复包
 
 # rm -rf feeds/luci/applications/luci-app-netdata
-# rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf package/small-package/luci-app-openvpn-server
 rm -rf package/small-package/openvpn-easy-rsa-whisky
 rm -rf package/small-package/luci-app-wrtbwmon
 rm -rf package/small-package/wrtbwmon
 rm -rf package/small-package/luci-app-koolproxyR
 rm -rf package/small-package/luci-app-godproxy
-rm -rf package/small-package/luci-app-argon-config
+rm -rf package/small-package/luci-app-argon*
 rm -rf package/small-package/luci-app-amlogic
 
 
