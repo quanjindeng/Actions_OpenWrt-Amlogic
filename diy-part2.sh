02704f17cfdc71206491d4a3a862924a65a05334
quanjindeng <54259993+quanjindeng@users.noreply.github.com>
2022-03-28 18:39:16 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,28 +10,6 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-######## 备用（暂不生效） ########
-# 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
-# 如果你的OP是当主路由的话，网关、DNS、广播都不需要，代码前面加 # 注释掉，只保留后台地址和子网掩码就可以
-# 如果你有编译ipv6的话，‘去掉LAN口使用内置的 IPv6 管理’代码前面也加 # 注释掉
-
-cat >package/base-files/files/etc/networkip <<-EOF
-uci set network.lan.ipaddr='10.10.10.1'                                    # IPv4 地址(openwrt后台地址)
-uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
-#uci set network.lan.gateway='10.10.10.1'                                   # IPv4 网关
-#uci set network.lan.broadcast='10.10.10.255'                               # IPv4 广播
-#uci set network.lan.dns='119.29.29.29 223.5.5.5'                         # DNS(多个DNS要用空格分开)
-#uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
-uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
-#uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
-#uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
-uci set system.@system[0].hostname='OpenWrt-N1'                             # 修改主机名称为OpenWrt-N1
-sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd             # 设置ttyd免帐号登录，如若开启，进入OPENWRT后可能要重启一次才生效
-EOF
-######## 备用（暂不生效） ########
-
-
-
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings    # 设置密码为空
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
@@ -83,10 +61,11 @@ rm -rf package/small-package/luci-app-godproxy
 rm -rf package/small-package/luci-app-argon*
 rm -rf package/small-package/luci-theme-argon*
 rm -rf package/small-package/luci-app-amlogic
+rm -rf package/small-package/luci-app-unblockneteasemusic
 
 
 # 其他调整
-NAME=$"package/small-package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
+NAME=$"package/luci-app-unblockneteasemusic/root/usr/share/unblockneteasemusic" && mkdir -p $NAME/core
 curl 'https://api.github.com/repos/UnblockNeteaseMusic/server/commits?sha=enhanced&path=precompiled' -o commits.json
 echo "$(grep sha commits.json | sed -n "1,1p" | cut -c 13-52)">"$NAME/core_local_ver"
 curl -L https://github.com/UnblockNeteaseMusic/server/raw/enhanced/precompiled/app.js -o $NAME/core/app.js
