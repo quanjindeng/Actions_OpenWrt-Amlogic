298e77f5f41269aaed980276757f099c85057fa3
quanjindeng <54259993+quanjindeng@users.noreply.github.com>
2023-04-05 12:35:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -64,6 +64,8 @@ rm -rf package/small-package/luci-app-argon*
 rm -rf package/small-package/luci-theme-argon*
 rm -rf package/small-package/luci-app-amlogic
 rm -rf package/small-package/luci-app-unblockneteasemusic
+rm -rf package/small-package/upx-static
+rm -rf package/small-package/upx
 
 
 # 其他调整
