c3a97dd2297b69fbe707a245c813ea6c8aeb87c9
quanjindeng <54259993+quanjindeng@users.noreply.github.com>
2023-04-09 19:02:16 +0800

    Update diy-part2-istore.sh

--- diy-part2-istore.sh
+++ diy-part2-istore.sh
@@ -35,6 +35,8 @@ sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 
 sed -i 's/invalid users = root/#invalid users = root/g' feeds/packages/net/samba4/files/smb.conf.template
 
+# 修复部分插件自启动脚本丢失可执行权限问题
+sed -i '/exit 0/i\chmod +x /etc/init.d/*' package/lean/default-settings/files/zzz-default-settings
 
 # 拉取软件包
 
